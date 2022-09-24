dacfb1bc1510e305d9d35af9ed46164492bd763e
Yuri Pimentel <yuripimentel@MacBook-Pro-de-Yuri.local>
2022-09-24 12:44:15 -0300

    ïniciando o projeto

---
+++ projetoFundamentosReactjs/src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from "./Post"
+
+export function App() {
+  
+  return (
+    <div>
+      <Post 
+        author="Yuri Pimentel" 
+        content="Um exemplo de texto"
+      />
+      <Post 
+        author="Victoria Beatriz" 
+        content="Um exemplo de texto"
+      />
+    </div>
+  )
+}
+
+
---
+++ projetoFundamentosReactjs/src/Post.jsx
@@ -0,0 +1,10 @@
+// propos = { author : "", content: ""}
+
+export function Post(props) {
+    return (
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </div>
+    )
+}
